fac104bce3187d02683d7aed9f56480451c44e21
Alex <106204990+AlexG2G@users.noreply.github.com>
2022-10-27 18:15:39 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import streamlit as st
 from utils import read_data, head, body
 
 st.set_page_config(
-    page_title='Mean, Median and Mode',
+    page_title='Mean, Median and Mode Calculator',
 )
  
-
 head()
 
-if st.button('Calculate Mean, Median and Mode for Dataset above'):
+if st.button('Calculate Mean, Median and Mode for the Dataset'):
     df = read_data('list1.csv')
     choice = df.sample(10)
     body(choice)
